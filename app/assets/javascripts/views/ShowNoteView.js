0ac03381d24f185cb96d9a11879111c93f7b5445
Nick & Brian <Nick_&_Brian@appacademy.io>
2013-05-08 15:13:57 -0700

    midcrusass

--- app/assets/javascripts/views/ShowNoteView.js
+++ app/assets/javascripts/views/ShowNoteView.js
@@ -24,142 +24,97 @@ ShowNoteView = Backbone.View.extend({
 
     rangy.init();
 
-    var i = 0;
-    _.each(LLC.comments.models, function(comment) {
+    // var i = 0;
+    LLC.comments.each(function(comment) {
       that['commentStyler' + comment.id] = rangy.createCssClassApplier("comment comment" + comment.id);
       var range = rangy.deserializeRange(comment.attributes.range);
       that['commentStyler' + comment.id].applyToRange(range);
 
-      i++;
+      // i++;
     })
 
-    var m = 0;
-    _.each(LLC.revisions.models, function(revision) {
-      that['revisionStyler' + m] = rangy.createCssClassApplier("revision revision" + m);
+    // var m = 0;
+    LLC.revisions.each(function(revision) {
+      that['revisionStyler' + revision.id] = rangy.createCssClassApplier("revision revision" + revision.id);
       var range = rangy.deserializeRange(revision.attributes.range);
-      that['revisionStyler' + m].applyToRange(range);
+      that['revisionStyler' + revision.id].applyToRange(range);
 
-      m++;
+      // m++;
     })
 
-    var j = 0;
-    _.each(LLC.comments.models, function(comment) {
+    // var j = 0;
+    LLC.comments.each(function(comment) {
       var $commentedRange =  $('.comment' + comment.id);
       var $commentPocket = $('<span class="commentPocket"></span>');
       $commentedRange.prepend($commentPocket);
-      that.setReviewListener(that, $commentedRange, $commentPocket, j, true);
+      that.setReviewListener(that, $commentedRange, $commentPocket, comment.id, true);
 
-      j++;
+      // j++;
     })
 
-    var n = 0;
-    _.each(LLC.revisions.models, function(revision) {
-      var $revisionedRange =  $('.revision' + n);
+    // var n = 0;
+    LLC.revisions.each(function(revision) {
+      var $revisionedRange =  $('.revision' + revision.id);
       var $revisionPocket = $('<span class="revisionPocket"></span>');
       $revisionedRange.prepend($revisionPocket);
-      that.setReviewListener(that, $revisionedRange, $revisionPocket, n, false);
+      that.setReviewListener(that, $revisionedRange, $revisionPocket, revisionId, false);
 
-      n++;
+      // n++;
     })
+
+    // var i = 0;
+    // _.each(LLC.comments.models, function(comment) {
+    //   that['commentStyler' + comment.id] = rangy.createCssClassApplier("comment comment" + comment.id);
+    //   var range = rangy.deserializeRange(comment.attributes.range);
+    //   that['commentStyler' + comment.id].applyToRange(range);
+    //
+    //   i++;
+    // })
+    //
+    // var m = 0;
+    // _.each(LLC.revisions.models, function(revision) {
+    //   that['revisionStyler' + m] = rangy.createCssClassApplier("revision revision" + m);
+    //   var range = rangy.deserializeRange(revision.attributes.range);
+    //   that['revisionStyler' + m].applyToRange(range);
+    //
+    //   m++;
+    // })
+    //
+    // var j = 0;
+    // _.each(LLC.comments.models, function(comment) {
+    //   var $commentedRange =  $('.comment' + comment.id);
+    //   var $commentPocket = $('<span class="commentPocket"></span>');
+    //   $commentedRange.prepend($commentPocket);
+    //   that.setReviewListener(that, $commentedRange, $commentPocket, j, true);
+    //
+    //   j++;
+    // })
+    //
+    // var n = 0;
+    // _.each(LLC.revisions.models, function(revision) {
+    //   var $revisionedRange =  $('.revision' + n);
+    //   var $revisionPocket = $('<span class="revisionPocket"></span>');
+    //   $revisionedRange.prepend($revisionPocket);
+    //   that.setReviewListener(that, $revisionedRange, $revisionPocket, n, false);
+    //
+    //   n++;
+    // })
+
   },
 
-  setReviewListener: function(that, $reviewedRange, $reviewPocket, k, isComment) {
+  setReviewListener: function(that, $reviewedRange, $reviewPocket, commentId, isComment) {
     var that = this;
 
     $reviewedRange.on('mouseover',
-      that.showReviewText.bind(that, $reviewedRange, $reviewPocket, k, isComment));
+      that.showReviewText.bind(that, $reviewedRange, $reviewPocket, commentId, isComment));
   },
 
-  showReviewText: function($reviewedRange, $reviewPocket, k, isComment) {
+  showReviewText: function($reviewedRange, $reviewPocket, commentId, isComment) {
     // var reviewText = isComment ? LLC.comments.get(k).get('body') : LLC.revisions.get(k).get('body')
-    var reviewText = isComment ? LLC.comments.models[k].attributes.body : LLC.revisions.models[k].attributes.body
+    var reviewText = isComment ? LLC.comments.get(commentId).get('body') : LLC.revisions.models[commentId].attributes.body
     $reviewPocket.append('<span class=reviewText>' + reviewText + '</span>');
     $reviewedRange.on('mouseout', function() {
       $reviewPocket.empty();
     })
   },
-
-  // showComments: function(note) {
-  //   var that = this;
-  //
-  //   // var note;
-  //   // if(note == null) {
-  //   //   note =
-  //   // }
-  //
-  //   rangy.init();
-  //   var i = 0;
-  //   _.each(LLC.comments.models, function(comment) {
-  //     that.commentStyler = rangy.createCssClassApplier("comment comment" + i);
-  //     var range = rangy.deserializeRange(comment.attributes.range);
-  //     that.commentStyler.applyToRange(range);
-  //
-  //     i++;
-  //   })
-  //
-  //   var j = 0;
-  //   _.each(LLC.comments.models, function(comment) {
-  //     var $commentedRange =  $($el).find('.comment' + j);
-  //     var $commentPocket = $('<span class="commentPocket"></span>');
-  //     $commentedRange.prepend($commentPocket);
-  //     that.setCommentListener(that, $commentedRange, $commentPocket, j);
-  //
-  //     j++;
-  //   })
-  // },
-  //
-  // setCommentListener: function(that, $commentedRange, $commentPocket, j) {
-  //   var that = this;
-  //
-  //   $commentedRange.on('mouseover',
-  //     that.showCommentText.bind(that, $commentedRange, $commentPocket, j));
-  // },
-  //
-  // showCommentText: function($commentedRange, $commentPocket, j) {
-  //   var commentText = LLC.comments.models[j].attributes.body;
-  //   $commentPocket.append('<span class=commentText>' + commentText + '</span>');
-  //   $commentedRange.on('mouseout', function() {
-  //     $commentPocket.empty();
-  //   })
-  // },
-  //
-  // showRevisions: function(note) {
-  //   var that = this;
-  //
-  //   rangy.init();
-  //   var i = 0;
-  //   _.each(LLC.revisions.models, function(revision) {
-  //     that.revisionStyler = rangy.createCssClassApplier("revision revision" + i);
-  //     console.log(revision.attributes.range);
-  //     var range = rangy.deserializeRange(revision.attributes.range);
-  //     that.revisionStyler.applyToRange(range);
-  //
-  //     i++;
-  //   })
-  //
-  //   var j = 0;
-  //   _.each(LLC.revisions.models, function(revision) {
-  //     var $revisionedRange =  $('.revision' + j);
-  //     var $revisionPocket = $('<span class="revisionPocket"></span>');
-  //     $revisionedRange.prepend($revisionPocket);
-  //     that.setRevisionListener(that, $revisionedRange, $revisionPocket, j);
-  //
-  //     j++;
-  //   })
-  // },
-  //
-  // setRevisionListener: function(that, $revisionedRange, $revisionPocket, j) {
-  //   var that = this;
-  //
-  //   $revisionedRange.on('mouseover',
-  //     that.showRevisionText.bind(that, $revisionedRange, $revisionPocket, j));
-  // },
-  //
-  // showRevisionText: function($revisionedRange, $revisionPocket, j) {
-  //   var revisionText = LLC.revisions.models[j].attributes.body;
-  //   $revisionPocket.append('<span class=revisionText>' + revisionText + '</span>');
-  //   $revisionedRange.on('mouseout', function() {
-  //     $revisionPocket.empty();
-  //   })
-  // }
 })
